326ac239cad1f526b5c49e3f75b8d30d9ccc7fd5
Mateusz <mateuszzygadlo00@gmail.com>
2021-07-29 21:03:11 +0200

    Edit main.js

--- main.js
+++ main.js
@@ -1,46 +1,71 @@
-function playRound(){
+const game = ['rock', 'paper', 'scissors'];
 
-    const game = ['rock', 'paper', 'scissors'];
-
+function computerPlay(){
     function mathRandom(parametr){
         return Math.floor(Math.random()*parametr.length);
     }
     function computerPlay(item){
         return item[mathRandom(game)];
     }
-    
-    const playerPlays = 'paper';
-    
+    return computerPlay(game);
+}
+
+let computerCount = 0;
+let playerCount = 0;
+let drawCount = 0;
+
+function playRound() {
+
     const computerPlays = computerPlay(game);
-    
-    function add(){
-        return computerPlays.concat(playerPlays);
-    }
+    const playerPlays = 'paper';
 
-    const result = add();
-    
-    function findWin(){
+    let result = computerPlays.concat(playerPlays);
 
-        switch(result){
-            case 'scissorspaper':
-            case 'rockscissprs':
-            case 'paperrock':
-                return `Computer play ${computerPlays} and win with player play ${playerPlays}\n`;
-                break;
-            case 'scissorsrock':
-            case 'rockpaper':
-            case 'paperscissors':
-                return `Player play ${playerPlays} and win with computer play ${computerPlays}\n`;
-                break;
-            case 'paperpaper':
-            case 'rockrock':
-            case 'scissorsscissors':
-                return `Computer play ${computerPlays} and player play ${playerPlays} and not wins\n`;
-                break;
-        }
+    switch(result){
+        case 'scissorspaper':
+        case 'rockscissprs':
+        case 'paperrock':
+            computerCount++;
+            return `Computer play ${computerPlays} and win with player play ${playerPlays}\n`;
+        case 'scissorsrock':
+        case 'rockpaper':
+        case 'paperscissors':
+            playerCount++;
+            return `Player play ${playerPlays} and win with computer play ${computerPlays}\n`;
+        case 'paperpaper':
+        case 'rockrock':
+        case 'scissorsscissors':
+            drawCount++;
+            return `Computer play ${computerPlays} and player play ${playerPlays} and not wins\n`;
     }
-    return findWin();
 }
-for(let i = 0; i < 15; i++){
-    console.log(playRound());
+function gameOn(){
+    for(let i = 0; i < 5; i++){
+        console.log(playRound());
+    }   
+}   
+
+function end(){
+    gameOn();
+    if(computerCount > playerCount){
+        return `winner is computer with ${computerCount} counts\n
+        player ${playerCount},draw ${drawCount}`;
+    }else if(playerCount > computerCount){
+        return `winner is player with ${playerCount} counts\n
+        computer ${computerCount} draw ${drawCount}`;
+    }
 }
+console.log(end());
+
+function clearValue(){
+    return computerCount = 0;
+}
+function clearValue2(){
+    return playerCount = 0;
+}
+function clearValue3(){
+    return drawCount = 0;
+}
+clearValue();
+clearValue2();
+clearValue3();
