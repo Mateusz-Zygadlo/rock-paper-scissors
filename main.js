ac1962e4ad0ecfd7180c9c98ce6e2558d25ac514
Mateusz <mateuszzygadlo00@gmail.com>
2021-08-01 18:32:45 +0200

    very good working main.js

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
 const choice = ['rock', 'paper', 'scissors'];
+let userChoice;
 
-function computerChoice(){
+let computerScore = 0;
+let playerScore = 0;
+
+let buttons = document.querySelectorAll('.button');
+
+let computerChoice = document.querySelector('.computer');
+let playerChoice = document.querySelector('.player');
+
+let computerCount = document.querySelector('.computerCount');
+let playerCount = document.querySelector('.playerCount');
+
+let winner = document.querySelector('.winner');
+
+function computerPlay(){
     function mathRandom(parametr){
         return Math.floor(Math.random()*parametr.length);
     }
-    function computerPlay(item){
-        return item[mathRandom(choice)];
-    }
-    return computerPlay(choice);
+    return choice[mathRandom(choice)];
 }
-console.log(computerChoice());
+function playRound() {
+
+    let computer = computerPlay();
+    computerChoice.textContent = computer;
+    let player = userChoice;
+
+    let result = computer.concat(player);
+
+    switch(result){
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            computerScore++;
+            computerCount.textContent = computerScore;
+            winner.textContent = 'computer win player lose';
+            break;
+        case 'scissorsrock':
+        case 'rockpaper':
+        case 'paperscissors':
+            playerScore++;
+            playerCount.textContent = playerScore;
+            winner.textContent = 'player win computer lose';
+            break;
+        case 'paperpaper':
+        case 'rockrock':
+        case 'scissorsscissors':
+            winner.textContent = 'computer player draw';
+            console.log('draw');
+            break;
+    }
+
+}
+
+buttons.forEach( (buttons) => {
+    buttons.addEventListener('click', (e) => {
+        userChoice = e.target.value;
+        playerChoice.textContent = userChoice;
+        playRound();
+    })
+})
